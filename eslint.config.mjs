5474056f3047993355a269189352e589aaa20b1f
Westley Cooper-Thorn <westleyct@me.com>
2026-06-15 12:58:22 +0100

    feat(boilerplate): npm init, vitest, eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,13 @@
+import tseslint from '@typescript-eslint/eslint-plugin'
+import tsparser from '@typescript-eslint/parser'
+
+export default [
+  {
+    files: ['**/*.ts'],
+    languageOptions: { parser: tsparser },
+    plugins: { '@typescript-eslint': tseslint },
+    rules: {
+      ...tseslint.configs.recommended.rules,
+    },
+  },
+]
